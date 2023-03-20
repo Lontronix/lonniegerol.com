f3e2e3304af8d442ef29305227a4d935ce447b01
Lonnie Gerol <lonnie@lonniegerol.com>
2023-03-19 20:36:30 -0400

    Adds opengraph attributes and page titles

--- src/routes/blog/post.svelte
+++ src/routes/blog/post.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { MetaTags } from 'svelte-meta-tags';
 
 	export let title
 	export let date
@@ -7,6 +8,21 @@
 	const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 </script>
 
+<MetaTags
+	title="{title}"
+	description="Lonnie's Corner of the Internet"
+	openGraph={{
+		type: "website",
+		title: title,
+		description: "Lonnie's Corner of the Internet",
+		images: [{
+			url: "https://s3.csh.rit.edu/lontronix-website/lonnie-hero.jpeg",
+			width: 2400,
+			height: 1260,
+			alt: "Lonnie Standing on the peak of a mountain"
+		}]
+	}}
+/>
 <article>
 	<div class="front-matter">
 		<h1 class="post-title">{title}</h1>
